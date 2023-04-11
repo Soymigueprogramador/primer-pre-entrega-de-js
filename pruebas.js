aafd93f9f9170e01ad711a77903083ee9b7a906a
Miguel Salazar <yobebe@hotmail.com.ar>
2023-04-10 23:59:29 -0300

    mi primer entrega de js

---
+++ pruebas.js
@@ -0,0 +1,37 @@
+//condicional: en esta condicional voy a poner el valor de la pagina. 
+const valorPagina = 1000; 
+//variables: aca voy a declarar las variables. 
+let cantidadDePaginas = parseInt(prompt("ingrese la cantidad de paginas que necesita para su sitio web"));
+let costoTotal = valorPagina * cantidadDePaginas;
+//ciclo y condicionales: aca esta un ciclo con los condicionales dentro.
+while(cantidadDePaginas) {
+    if(cantidadDePaginas >= 6) {
+    costoTotal = costoTotal * 2;
+    alert("te sale 12000");
+}
+else if(cantidadDePaginas === 2){
+    alert("te sale 2000");
+}
+else if(cantidadDePaginas === 3){
+    alert("te sale 3000");
+}
+else if(cantidadDePaginas === 4){
+    alert("te sale 4000");
+}
+else if(cantidadDePaginas === 5){
+    alert("te sale 5000");
+}
+{
+    break
+}
+}
+console.log("gracias por elegirnos, te pedimos que te pongas en contacto para mas informacion")
+//funcion: esta funcion me deberia mostrar que opcion eligio el usuario.
+function esLoQueEligio() {
+    return "esLoQueQuiereElUsuario";
+} 
+let miraLoQueQuiero = "esLoQueQuiereElUsuario";
+esLoQueEligio();
+console.log(miraLoQueQuiero);
+//en esta funcion mi idea era que el usuario elija una opcion y que en la consola me mostrase cual era esa opcion pero no supe como hacerla,
+//quedara para la proxima.
